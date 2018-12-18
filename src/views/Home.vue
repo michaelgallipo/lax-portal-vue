67f120a24ada31f7459f384386c9cbedef491d18
michael <michael.gallipo@gmail.com>
2018-12-18 15:23:57 -0500

    Modified home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/howbigyouplay.jpg">
-    <HelloWorld msg="Welcome to New Hampshire Lacrosse Portal"/>
+<!--     <HelloWorld msg="Welcome to New Hampshire Lacrosse Portal"/> -->
+    <h1>Welcome to New Hampshire Lacrosse Portal</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+// export default {
+//   name: 'home',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
